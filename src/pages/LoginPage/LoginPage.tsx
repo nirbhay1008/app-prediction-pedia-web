95aa3d13c5544ba69d3f0a6fe1fc618c55580ac6
Nirbhay Chopra <nirbhaychopra1008@gmail.com>
2025-04-26 19:07:27 +0530

    test commit

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -13,7 +13,7 @@ export const LoginPage = ({
   return (
     <div className="login-container">
       <motion.div
-        initial={{ opacity: 0, y: -50 }}
+        initial={{ opacity: 0, y: -50 }} 
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
       >
